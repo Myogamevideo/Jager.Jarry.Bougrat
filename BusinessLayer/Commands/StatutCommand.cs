ed518bbbb9dd17e7c18d7501403ce1c5a2e02288
AllanJRY <jarry.allan@gmail.com>
2021-06-05 11:57:16 +0200

    impl: StatutCommand

---
+++ BusinessLayer/Commands/StatutCommand.cs
@@ -0,0 +1,48 @@
+﻿using ClassLibrary;
+using ClassLibrary.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Commands
+{
+    class StatutCommand
+    {
+        private readonly Context _context;
+
+        public StatutCommand(Context context)
+        {
+            _context = context;
+        }
+
+        public int Add(Statut statut)
+        {
+            _context.Statut.Add(statut);
+            return _context.SaveChanges();
+        }
+
+        public void Update(Statut statut)
+        {
+            Statut updatedStatut = _context.Statut.Where(s => s.Id == statut.Id).FirstOrDefault();
+            if (updatedStatut != null)
+            {
+                // TODO
+            }
+
+            _context.SaveChanges();
+        }
+
+        public void Delete(int statutID)
+        {
+            Statut statutToDel = _context.Statut.Where(o => o.Id == statutID).FirstOrDefault();
+            if (statutToDel != null)
+            {
+                _context.Statut.Remove(offreToDel);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
